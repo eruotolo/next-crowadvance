1099e46d07e7e4f32f3bc31684b83ab13f6f3139
Edgardo Ruotolo <edgardoruotolo@gmail.com>
2025-01-30 01:45:26 -0300

    Task: Politicas Version: 1.7 Date: 30-01-2025

---
+++ src/app/privacity/page.jsx
@@ -0,0 +1,208 @@
+export const metadata = {
+    title: 'Privacidad de la Información | Crow Advance',
+    alternates: {
+        canonical: 'https://www.crowadvance.com/privacity',
+        language: {
+            es: '/',
+        },
+    },
+};
+
+export default function PrivacyPolicy() {
+    return (
+        <main>
+            <section className="py-[120px] pb-[40px] md:pt-[150px] md:pb-[70px]">
+                <div className="container mx-auto px-[20px] md:px-[32px]">
+                    <div className="flex flex-col">
+                        <h1 className="text-negros text-[26px] leading-[30px] font-bold xl:text-[56px] xl:leading-[60px] 2xl:text-[70px] 2xl:leading-[70px]">
+                            POLÍTICA DE PRIVACIDAD
+                        </h1>
+
+                        <h4 className="text-negro mt-[20px] mb-[10px] font-medium md:my-[30px] md:text-[28px]">
+                            IDENTIFICACIÓN DEL RESPONSABLE DEL TRATAMIENTO
+                        </h4>
+
+                        <p className="text-negro font-normal md:text-[18px] md:leading-[32px]">
+                            <b className="text-purpura">Identidad:</b> Crow Advance SPA
+                        </p>
+                        <p className="text-negro font-normal md:text-[18px] md:leading-[32px]">
+                            <b className="text-purpura">Dirección:</b> Antonio Guarategua Lebe |
+                            Chiloé - Castro
+                        </p>
+                        <p className="text-negro font-normal md:text-[18px] md:leading-[32px]">
+                            <b className="text-purpura">Teléfono:</b> +56 9 6755 3841
+                        </p>
+                        <p className="text-negro font-normal md:text-[18px] md:leading-[32px]">
+                            <b className="text-purpura">Correo electrónico:</b> hola@crowadvance.com
+                        </p>
+
+                        <h4 className="text-negro mt-[20px] mb-[10px] font-medium md:my-[30px] md:text-[28px]">
+                            FINALIDAD DEL TRATAMIENTO DE SUS DATOS PERSONALES
+                        </h4>
+                        <p className="text-negro font-normal md:text-[18px] md:leading-[32px]">
+                            Esta página web ha sido creada por CROW ADVANCE con finalidad
+                            informativa y para uso personal. En este sentido, la finalidad del
+                            tratamiento será la solicitud de información realizada a través de la
+                            web ya sea a través del formulario de contacto creado al efecto, o
+                            mediante una solicitud mediante correo electrónico, así como gestionar
+                            cualquier otra interacción entre el usuario del sitio web y CROW
+                            ADVANCE, SPA, que tiende a satisfacer la necesidad del usuario. Toda la
+                            información solicitada es necesaria para poder atender la solicitud.
+                        </p>
+                        <p className="text-negro font-normal md:text-[18px] md:leading-[32px]">
+                            El acceso a este sitio web puede implicar el uso de cookies. Las cookies
+                            son pequeñas cantidades de información que se almacenan en el navegador
+                            utilizado por el usuario, para que el servidor registre información que
+                            pueda ser utilizada en accesos posteriores. La información almacenada
+                            permite identificar a un usuario concreto y tener registrado información
+                            sobre sus preferencias, páginas visitadas, etc. Si no desea permitir la
+                            instalación de cookies en su ordenador, deberá configurar su navegador a
+                            tal efecto.
+                        </p>
+
+                        <h4 className="text-negro mt-[20px] mb-[10px] font-medium md:my-[30px] md:text-[28px]">
+                            PERIODO DE ALMACENAMIENTO
+                        </h4>
+                        <p className="text-negro font-normal md:text-[18px] md:leading-[32px]">
+                            CROW ADVANCE procederá a la cancelación de los datos recabados a través
+                            de la web cuando ya no sean necesarios o pertinentes para la finalidad
+                            que fueron recabados o, en su defecto, cuando haya transcurrido el plazo
+                            de prescripción legal para las acciones correspondientes.
+                        </p>
+
+                        <h4 className="text-negro mt-[20px] mb-[10px] font-medium md:my-[30px] md:text-[28px]">
+                            COMUNICACIÓN O CESIÓN DE DATOS Y TRANSFERENCIA INTERNACIONAL DE DATOS
+                        </h4>
+                        <p className="text-negro font-normal md:text-[18px] md:leading-[32px]">
+                            CROW ADVANCE podrá comunicar sus datos personales a administraciones
+                            públicas con competencia en la materia, y si existe obligación legal de
+                            hacerlo.
+                        </p>
+                        <p className="text-negro font-normal md:text-[18px] md:leading-[32px]">
+                            CROW ADVANCE le informa que existen diversos prestadores de servicios
+                            que podrán acceder a sus datos para llevar a cabo la Orden de
+                            Tratamiento. El acceso se realizará con todas las medidas de seguridad
+                            necesarias para salvaguardar y proteger su información según lo regula
+                            el correspondiente contrato.
+                        </p>
+                        <p className="text-negro font-normal md:text-[18px] md:leading-[32px]">
+                            Dicha disposición no constituirá en ningún momento una cesión de datos.
+                            No realizamos ninguna transferencia internacional de tus datos
+                            personales
+                        </p>
+
+                        <h4 className="text-negro mt-[20px] mb-[10px] font-medium md:my-[30px] md:text-[28px]">
+                            DERECHOS DEL INTERESADO
+                        </h4>
+                        <p className="text-negro font-normal md:text-[18px] md:leading-[32px]">
+                            Cualquier persona tiene derecho a obtener confirmación sobre si en CROW
+                            ADVANCE, SPA estamos tratando datos personales que le conciernen y, en
+                            tal caso, derecho de acceso a sus datos personales, a los efectos del
+                            tratamiento, y a las categorías de datos personales de que se trate.
+                        </p>
+                        <p className="text-negro font-normal md:text-[18px] md:leading-[32px]">
+                            Asimismo, tendrá derecho a solicitar la rectificación de los datos
+                            inexactos o, en su caso, solicitar su supresión cuando los datos ya no
+                            sean necesarios para los fines para los que fueron recabados; se ha
+                            retirado el consentimiento en el que se basa el tratamiento o se opone
+                            al mismo; los datos personales han sido procesados ilegalmente; o deba
+                            ser suprimido en cumplimiento de una obligación legal establecida en el
+                            Derecho del Mercosur o de los Estados miembros.
+                        </p>
+                        <p className="text-negro font-normal md:text-[18px] md:leading-[32px]">
+                            Podrá solicitar la limitación del tratamiento de sus datos cuando
+                            impugnen la exactitud de los datos personales durante el plazo que
+                            permita al responsable del tratamiento verificar su exactitud; el
+                            tratamiento sea ilícito y el interesado se oponga a la supresión de los
+                            datos personales y solicite en su lugar la limitación de su uso; el
+                            responsable del tratamiento ya no necesita los datos personales para los
+                            fines del tratamiento, pero el interesado los necesita para la
+                            formulación, ejercicio o defensa de reclamaciones; o el interesado se ha
+                            opuesto al tratamiento, comprobando si los motivos legítimos del
+                            responsable del tratamiento prevalecen sobre los del interesado. En
+                            tales casos, únicamente conservaremos los datos para el ejercicio o la
+                            defensa de reclamaciones.
+                        </p>
+                        <p className="text-negro font-normal md:text-[18px] md:leading-[32px]">
+                            En determinadas circunstancias y por motivos relacionados con su
+                            situación particular, podrá oponerse al tratamiento de sus datos. CROW
+                            ADVANCE, SPA. dejará de tratar los datos, salvo por motivos legítimos
+                            imperiosos, o el ejercicio o la defensa de posibles reclamaciones
+                        </p>
+                        <p className="text-negro font-normal md:text-[18px] md:leading-[32px]">
+                            Tendrás derecho a recibir los datos personales que te incumban y que
+                            hayas facilitado, en un formato estructurado, y a transferirlos a otro
+                            responsable del tratamiento sin que se lo impida el responsable del
+                            tratamiento al que fueron facilitados, cuando el el procesamiento se
+                            basa en el consentimiento; o si el tratamiento se realiza por medios
+                            automatizados.
+                        </p>
+                        <p className="text-negro font-normal md:text-[18px] md:leading-[32px]">
+                            Los Usuarios pueden ejercer sus derechos dirigiendo una solicitud a la
+                            dirección de correo electrónico privacidad@crowadvance.com .
+                        </p>
+
+                        <h4 className="text-negro mt-[20px] mb-[10px] font-medium md:my-[30px] md:text-[28px]">
+                            ORIGEN
+                        </h4>
+                        <p className="text-negro font-normal md:text-[18px] md:leading-[32px]">
+                            Los datos personales que tratamos en CROW ADVANCE, SPA. proceden del
+                            propio interesado
+                        </p>
+
+                        <h4 className="text-negro mt-[20px] mb-[10px] font-medium md:my-[30px] md:text-[28px]">
+                            INFORMACIÓN ADICIONAL
+                        </h4>
+                        <p className="text-negro font-normal md:text-[18px] md:leading-[32px]">
+                            Los usuarios garantizan y responden, en cualquier caso, de la veracidad,
+                            exactitud, vigencia, autenticidad de los Datos Personales facilitados, y
+                            se comprometen a mantenerlos adecuadamente actualizados. Asimismo, los
+                            usuarios garantizan ser mayores de 14 años.
+                        </p>
+                        <p className="text-negro font-normal md:text-[18px] md:leading-[32px]">
+                            En el caso de que los datos personales sean facilitados por personas
+                            distintas del interesado, el usuario deberá informar a dichas personas
+                            de los extremos contenidos en los párrafos anteriores con carácter
+                            previo a su inclusión. En estos casos, el usuario garantiza que la
+                            información reportada pertenece a personas mayores de 14 años y que la
+                            información es exacta y veraz. CROW ADVANCE, SPA. quedará exenta de
+                            cualquier responsabilidad derivada del incumplimiento por parte del
+                            usuario de estos requisitos.
+                        </p>
+                        <p className="text-negro font-normal md:text-[18px] md:leading-[32px]">
+                            CROW ADVANCE, SPA. ha adoptado los niveles de seguridad de Protección de
+                            Datos Personales legalmente requeridos y ha instalado todos los medios y
+                            medidas técnicas a su alcance según el estado de la tecnología para
+                            evitar la pérdida, mal uso, alteración, acceso no autorizado y robo de
+                            los Datos Personales facilitados.
+                        </p>
+                        <p className="text-negro font-normal md:text-[18px] md:leading-[32px]">
+                            El usuario tiene el deber de confidencialidad y secreto de los empleados
+                            de CROW ADVANCE, SPA y de todos aquellos que traten los datos en su
+                            nombre y por cuenta de ellos. Sin perjuicio de lo anterior, el usuario
+                            es consciente de la posibilidad de que la seguridad de las
+                            comunicaciones a través de la red no sea invulnerable.
+                        </p>
+
+                        <h4 className="text-negro mt-[20px] mb-[10px] font-medium md:my-[30px] md:text-[28px]">
+                            JURISDICCIÓN Y LEYES APLICABLES
+                        </h4>
+                        <p className="text-negro font-normal md:text-[18px] md:leading-[32px]">
+                            Los términos y condiciones que regulan este sitio web y todas las
+                            relaciones que del mismo puedan derivarse están protegidos por la ley
+                            chilena.
+                        </p>
+                        <p className="text-negro font-normal md:text-[18px] md:leading-[32px]">
+                            Cualquier controversia que pueda surgir del acceso o uso de este sitio
+                            web será competencia de los tribunales que correspondan según la
+                            jurisdicción del usuario.
+                        </p>
+                        <p className="text-negro font-normal md:mt-[20px] md:text-[18px] md:leading-[32px]">
+                            © Copyright CROW ADVANCE, SPA 2024. Todos los derechos reservados
+                        </p>
+                    </div>
+                </div>
+            </section>
+        </main>
+    );
+}
